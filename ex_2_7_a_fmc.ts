d0767afc3273ceadbca54c83bddfb0725955f0e5
Franciele Chesere <126211988+ChesereF@users.noreply.github.com>
2023-08-30 21:50:11 -0300

    Create ex_2_7_a_fmc.ts

---
+++ ex_2_7_a_fmc.ts
@@ -0,0 +1,27 @@
+class VeiculoEletrico extends Veiculo {
+    bateria: number;
+
+    constructor(longitude: number, latitude: number, bateria: number) {
+        super(longitude, latitude);
+        this.bateria = bateria;
+    }
+
+    carregarBateria(novaCarga: number) {
+        this.bateria += novaCarga;
+    }
+}
+
+class VeiculoEletricoVoador extends VeiculoEletrico {
+    altitude: number;
+
+    constructor(longitude: number, latitude: number, bateria: number, altitude: number) {
+        super(longitude, latitude, bateria);
+        this.altitude = altitude;
+    }
+
+    alterarAltitude(novaAltitude: number) {
+        this.altitude = novaAltitude;
+    }
+}
+
+export { Veiculo, VeiculoEletrico, VeiculoEletricoVoador };
